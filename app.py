ef9d09ee10bb5f7d89f56814056157a247d3ba08
Taylan Şirinoğlu <taylansirinoglu@gmail.com>
2026-03-12 00:15:19 +0300

    Update app.py
    Add city ve delete fonksiyonları eklendi

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
  
 app = Flask(__name__)
  
+favorite_cities = []
+ 
 @app.route("/hello")
-
 def hello():
-
-    return "Hello! Welcome to Favorite City App"
+    return "Welcome to Favorite City App"
+ 
+@app.route("/add/<city>")
+def add_city(city):
+    favorite_cities.append(city)
+    return f"{city} added!"
+ 
+@app.route("/cities")
+def cities():
+    return jsonify(favorite_cities)
+ 
+@app.route("/delete/<city>")
+def delete_city(city):
+    if city in favorite_cities:
+        favorite_cities.remove(city)
+        return f"{city} deleted!"
+    return "City not found"
  
 if __name__ == "__main__":
-
     app.run(debug=True)
- 
